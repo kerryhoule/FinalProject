73e73039f400a613c8cd42bbd3b7012d4b03700c
Kerry Houle <khoule38@academic.rrc.ca>
2013-04-10 17:41:20 -0500

    Search working, partials created for search.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -45,7 +45,6 @@ class HomeController < ApplicationController
 
   def search_results
   	keywords = params[:keywords]
-  	@customers = Customer.where("first_name LIKE ? OR last_name LIKE ?", "%#{keywords}%", "%#{keywords}%")
   	@contractors = Contractor.where("first_name LIKE ? OR last_name LIKE ?", "%#{keywords}%", "%#{keywords}%")
   	@services = Service.where("service_name LIKE ?", "%#{keywords}%")
   end
