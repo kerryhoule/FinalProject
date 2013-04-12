07f4b590d62e8010ec14e235e74a355b0e065bf0
Kerry Houle <khoule38@academic.rrc.ca>
2013-04-12 12:28:03 -0500

    Removed uneeded migration file.

--- db/migrate/20130405165525_rename_contractid_to_contractorid.rb
+++
@@ -1,9 +0,0 @@
-class RenameContractidToContractorid < ActiveRecord::Migration
-  def up
-  	rename_column :contractor_services, :contract_id, :contractor_id
-  end
-
-  def down
-  	rename_column :contractor_services, :contract_id, :contractor_id
-  end
-end
